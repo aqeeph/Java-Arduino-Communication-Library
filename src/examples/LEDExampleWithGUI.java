8c57b6bc24fd8ad4d6359d1e30a14ed644df0dff
Mehmet Akif İLYASOĞULLARI <mehmetakifilyasogullari@gmail.com>
2020-11-07 20:08:43 +0300

    url change
    dosya yolu isimleri değiştirildi.globalleştirilmesi lazım.

--- src/examples/LEDExampleWithGUI.java
+++ src/examples/LEDExampleWithGUI.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -23,86 +24,126 @@ public class LEDExampleWithGUI {
 	static JButton btnOn = new JButton("ON");
 	static JButton btnOff = new JButton("OFF");
 	static JButton btnRefresh;
-	
+
+	 static boolean is_open = false;
+	static String a = "";
+	static Thread t;
 	public static void main(String[] args) {
-		setUpGUI(); // refer to this function only if you have knowledge of JAVA swing classes and GUI elements.
-		
+		setUpGUI(); // refer to this function only if you have knowledge of JAVA swing classes and
+					// GUI elements.
+		arduino = new Arduino();
 		frame.setResizable(false);
 		
-		btnOn.addActionListener(new ActionListener(){
-			@Override public void actionPerformed(ActionEvent e) {
-				arduino.serialWrite('1');
-				
-				} 
-			});
+		btnOn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// arduino.serialWrite('1');
+				is_open = true;
+				//threade restart cekmek yerine her kullanilmasi gerektiginde yeniden olusturmak daha iyi performans verir.
+				//ayrica diger turlu nasil yapildigini bilmiyorum.
+				t = new Thread(new Runnable() {
+					public void run() {
+						// perform any operation
+						System.out.println("Performing operation in Asynchronous Task");						
+							while (is_open == true) {
+								try {
+									a = "" + arduino.serialRead();
+								} catch (NullPointerException e) {
+									e.printStackTrace();
+								}catch (Exception e) {
+									e.printStackTrace();
+								}
+								if (a != null) {
+									System.out.println("" + a);
+								}
+							}
+					}
+				});
+				t.start();
+			}
+		});
+
+		btnOff.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// arduino.serialWrite('0');
+				 is_open = false;
+			}
+		});
 		
-		btnOff.addActionListener(new ActionListener(){
-			@Override public void actionPerformed(ActionEvent e) {
-				arduino.serialWrite('0');
-				} 
-			});
-	
+		 
+		
+		
+		
 	}
-	
-	public static void populateMenu(){ //gets the list of available ports and fills the dropdown menu
+	public static byte[] hexStringToByteArray(String s) {
+	    int len = s.length();
+	    byte[] data = new byte[len / 2];
+	    for (int i = 0; i < len; i += 2) {
+	        data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+	                             + Character.digit(s.charAt(i+1), 16));
+	    }
+	    return data;
+	}
+	public static void populateMenu() { // gets the list of available ports and fills the dropdown menu
 		final PortDropdownMenu portList = new PortDropdownMenu();
 		portList.refreshMenu();
 		final JButton connectButton = new JButton("Connect");
-		
-		
-		ImageIcon refresh = new ImageIcon("/Users/HirdayGupta/Documents/workspace/Java-Arduino-Communication-Library/src/examples/refresh.png");
+
+		ImageIcon refresh = new ImageIcon(
+				"C:/Users/ÝZOLY GAMERS//Documents/GitHub/Java-Arduino-Communication-Library/src/examples/refresh.png");
 		btnRefresh = new JButton(refresh);
-		
+
 		JPanel topPanel = new JPanel();
-		
-		btnRefresh.addActionListener(new ActionListener(){
-			
+
+		btnRefresh.addActionListener(new ActionListener() {
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				portList.refreshMenu();
-			
-		}
+
+			}
 		});
 		topPanel.add(portList);
 		topPanel.add(btnRefresh);
 		topPanel.add(connectButton);
 		// populate the drop-down box
-		
-		connectButton.addActionListener(new ActionListener(){
+
+		connectButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(connectButton.getText().equals("Connect")){
-				 arduino = new Arduino(portList.getSelectedItem().toString(),9600);
-				 if(arduino.openConnection()){
-					 connectButton.setText("Disconnect");
-					 portList.setEnabled(false);
-					 btnOn.setEnabled(true);
-					 btnOff.setEnabled(true);
-					 btnRefresh.setEnabled(false);
-					 frame.pack();
-				 }
-				}
-				else {
+				if (connectButton.getText().equals("Connect")) {
+					arduino = new Arduino(portList.getSelectedItem().toString(), 9600);
+					if (arduino.openConnection()) {
+						connectButton.setText("Disconnect");
+						portList.setEnabled(false);
+						btnOn.setEnabled(true);
+						btnOff.setEnabled(true);
+						btnRefresh.setEnabled(false);
+						frame.pack();
+					}
+				} else {
 					arduino.closeConnection();
-					connectButton.setText("Connect");;
+					connectButton.setText("Connect");
+					;
 					portList.setEnabled(true);
 					btnOn.setEnabled(false);
 					btnRefresh.setEnabled(true);
 					btnOff.setEnabled(false);
 				}
 			}
-			
+
 		});
-		//topPanel.setBackground(Color.BLUE);
+		// topPanel.setBackground(Color.BLUE);
 		frame.add(topPanel, BorderLayout.NORTH);
 	}
-	
-	public static void setUpGUI(){
+
+	public static void setUpGUI() {
 		frame.setSize(600, 600);
 		frame.setBackground(Color.black);
 		frame.setForeground(Color.black);
-		//frame.setPreferredSize(new Dimension(600,600));
+		// frame.setPreferredSize(new Dimension(600,600));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(new BorderLayout());
 		btnOn.setForeground(Color.GREEN);
@@ -110,7 +151,7 @@ public class LEDExampleWithGUI {
 		btnOff.setForeground(Color.RED);
 		btnOff.setEnabled(false);
 		JPanel pane = new JPanel();
-		//pane.setBackground(Color.blue);
+		// pane.setBackground(Color.blue);
 		pane.add(btnOn);
 		pane.add(btnOff);
 		frame.add(pane, BorderLayout.CENTER);
@@ -118,6 +159,6 @@ public class LEDExampleWithGUI {
 		frame.pack();
 		frame.getContentPane();
 		frame.setVisible(true);
-		
+
 	}
 }
